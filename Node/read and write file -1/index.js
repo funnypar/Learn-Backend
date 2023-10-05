b1147cc7a174a7ff7ba2e50527a2b3aca64ae3ac
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-05 09:59:52 -0400

    Add files via upload

--- Node/read and write file -1/index.js
+++ Node/read and write file -1/index.js
@@ -1,6 +1,16 @@
+// --------------------module "File System"
+// const fs = require("fs");
+// Now i want to read the
+// --------------------const file = fs.readFileSync("./file.txt", "UTF-8");
+// Log it into consile
+// -------------------console.log(file);
+
+// ------------------------------------ Wrtie in file ------------------------------------
 // module "File System"
 const fs = require("fs");
-// Now i want to read the
-const file = fs.readFileSync("./file.txt", "UTF-8");
-// Log it into consile
-console.log(file);
+// what i want to add
+const myTxt = "Here we goooooo 🥊.\n I will see you again in the next step 😉";
+// write it in file
+fs.writeFileSync("./output.txt", myTxt);
+// show that was success
+console.log("Done!");
