ce247253a1702b5820f6cfbbcbe8a880c451709e
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-08 02:55:26 -0400

    implement simple routing

--- Node/create simple server -2/server.js
+++ Node/create simple server -2/server.js
@@ -1,8 +1,16 @@
 // require http library
 const http = require("http");
+const url = require("url");
 // create a server
 const server = http.createServer((req, res) => {
-    res.end("Hi from server 😉");
+    // Simple Routing
+    if (req.url == "/") {
+        res.end("Hi from server !");
+    } else if (req.url == "/parsam") {
+        res.end("Hi parsam !");
+    } else if (req.url == "/mopano") {
+        res.end("Hi mopano !");
+    }
 });
 // start server
 server.listen(8000, "127.0.0.1", () => {
