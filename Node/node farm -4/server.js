61aa1f179bd35517d4c504a0ff44ecc4ef838792
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-10 04:29:23 -0400

    overviewUp

--- Node/node farm -4/server.js
+++ Node/node farm -4/server.js
@@ -3,13 +3,32 @@ const http = require("http");
 const url = require("url");
 // ---------------------------------- Datas -----------------------------------------
 const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const overviewData = fs.readFileSync(
+    `${__dirname}/templates/overview.html`,
+    "utf-8"
+);
+const productData = fs.readFileSync(
+    `${__dirname}/templates/product.html`,
+    "utf-8"
+);
+const cardData = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
 // ---------------------------------- server ----------------------------------------
 const server = http.createServer((req, res) => {
+    // root
     if (req.url == "/") {
         res.end("Hi from server");
+        // api
     } else if (req.url == "/api") {
         res.writeHead(200, { "Content-type": "application/json" });
         res.end(apiData);
+        // overview
+    } else if (req.url == "/overview") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end(overviewData);
+        // not found
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.end("<h1>Page Not Found !!!</h1>");
     }
 });
 
