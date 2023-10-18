657a6af16d1e766965e6a8efed56a8c2f7c34611
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-18 15:02:36 -0400

    withStream

--- Node/stream -5/server.js
+++ Node/stream -5/server.js
@@ -2,10 +2,22 @@ const fs = require("fs");
 const server = require("http").createServer();
 // -------------------------------------- Server ------------------------------------
 server.on("request", (req, res) => {
-    // Without streams
-    fs.readFile("./text.txt", "utf-8", (err, data) => {
-        if (err) console.log("Error in file !!!");
-        res.end(data);
+    // ------------------------------ Without streams -------------------------------
+    // fs.readFile("./text.txt", "utf-8", (err, data) => {
+    //     if (err) console.log("Error in file !!!");
+    //     res.end(data);
+    // });
+    // ------------------------------ with Stream ----------------------------------
+    const readble = fs.createReadStream("./text.txt");
+    readble.on("data", (piece) => {
+        res.write(piece);
+    });
+    readble.on("error", (err) => {
+        res.writeHeader(500, { "Content-type": "text/html" });
+        res.end("<h1>File not found !</h1>");
+    });
+    readble.on("end", () => {
+        res.end();
     });
 });
 
