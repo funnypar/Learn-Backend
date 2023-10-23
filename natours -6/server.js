298d4c48fc5ecb032aeb5084c99f2f420463a8dd
Mopano <99732517+funnypar@users.noreply.github.com>
2023-10-23 03:09:56 -0400

    handlePost

--- natours -6/server.js
+++ natours -6/server.js
@@ -9,7 +9,8 @@ const tours = JSON.parse(
 );
 // ----------------------------------- App --------------------------------------
 const app = express();
-// routing
+app.use(express.json());
+// routing-get
 app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
         status: "success",
@@ -19,6 +20,34 @@ app.get("/api/v1/tours", (req, res) => {
             tours: tours,
         },
     });
+});
+// routing-post
+app.post("/api/v1/tours", (req, res) => {
+    // create new id
+    const newId = tours[tours.length - 1].id + 1;
+    // create new tour
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    // push newTour in tours
+    tours.push(newTour);
+    // write that on tours
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (err, data) => {
+            if (err) {
+                res.status(501).json({
+                    status: "not implemented",
+                    message: "There is an error !!!",
+                });
+            }
+            res.status(201).json({
+                status: "created",
+                date: new Date().toString(),
+                newTour: tours[tours.length - 1],
+            });
+        }
+    );
 });
 // listen on that
 app.listen(PORT, () => {
